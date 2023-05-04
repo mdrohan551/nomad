b052aa4cdd1aa6187ac18154281d963ed4497df5
mdarmanrohan <learningcell555@gmail.com>
2023-05-05 00:31:26 +0600

    fd

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ $(function () {
   $('#banner').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay:false,
+    autoplay:true,
     autoplaySpeed: 2000,
     arrows: false,
     dots: true,
